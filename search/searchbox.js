243fbc38276a150136f7b814be3a70e7cdb375e7
feeshy <10860212+feeshy@users.noreply.github.com>
2019-06-08 14:40:40 +0800

    update style

--- search/searchbox.js
+++
@@ -1,9 +0,0 @@
-﻿  (function() {
-    var cx = '002093486978024606124:caqnm_ayaoq';
-    var gcse = document.createElement('script');
-    gcse.type = 'text/javascript';
-    gcse.async = true;
-    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(gcse, s);
-  })();
